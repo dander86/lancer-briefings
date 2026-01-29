53864199ccab37745da7e82071b65fe9e900ccf5
dander86 <123803898+dander86@users.noreply.github.com>
2026-01-30 00:46:54 +0100

    fixing imports try 3

--- src/assets/LCPs/sotw-data-1.0.2/index.js
+++ src/assets/LCPs/sotw-data-1.0.2/index.js
@@ -2,14 +2,12 @@
 import f from "./frames.json"
 import p from "./pilot_gear.json"
 import s from "./systems.json"
-import t from "./talents.json"
 import b from "./bonds.json"
 
 const data = {
   frames: f,
   pilot_gear: p,
   systems: s,
-  talents: t,
   bonds: b,
 }
 
