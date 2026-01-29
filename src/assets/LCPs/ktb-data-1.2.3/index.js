03cd38ab8aac8d9f392707c5c06fd5394911de77
dander86 <123803898+dander86@users.noreply.github.com>
2026-01-30 00:43:41 +0100

    Fixing LCP import try 1

---
+++ src/assets/LCPs/ktb-data-1.2.3/index.js
@@ -0,0 +1,16 @@
+"use strict"
+import f from "./frames.json"
+import s from "./systems.json"
+import w from "./weapons.json"
+import t from "./talents.json"
+import b from "./bonds.json"
+
+const data = {
+  frames: f,
+  systems: s,
+  weapons: w,
+  talents: t,
+  bonds: b,
+}
+
+export default data;
---
+++ src/assets/LCPs/sotw-data-1.0.2/index.js
@@ -0,0 +1,18 @@
+"use strict"
+import f from "./frames.json"
+import p from "./pilot_gear.json"
+import s from "./systems.json"
+import w from "./weapons.json"
+import t from "./talents.json"
+import b from "./bonds.json"
+
+const data = {
+  frames: f,
+  pilot_gear: p,
+  systems: s,
+  weapons: w,
+  talents: t,
+  bonds: b,
+}
+
+export default data;
